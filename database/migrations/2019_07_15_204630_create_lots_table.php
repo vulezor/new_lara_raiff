3d829a1eca268772ffc3852602d004c7f50d00d1
Zoran <zvulanovic@yahoo.com>
2019-07-20 22:57:20 +0200

    New commits

---
+++ database/migrations/2019_07_15_204630_create_lots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lots', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->integer('id', 10)->unsigned();
+            $table->integer('name')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lots');
+    }
+}
